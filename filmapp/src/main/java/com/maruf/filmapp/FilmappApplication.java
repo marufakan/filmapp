512c3b6ef0283ef1abc79ca4f3eacac75a6f7823
Maruf Akan <50638375+marufakan@users.noreply.github.com>
2024-09-15 00:38:06 +0300

    Created FilmApp

---
+++ filmapp/src/main/java/com/maruf/filmapp/FilmappApplication.java
@@ -0,0 +1,13 @@
+package com.maruf.filmapp;
+
+import com.maruf.filmapp.domain.Film;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmappApplication.class, args);
+	}
+}
---
+++ filmapp/src/test/java/com/maruf/filmapp/FilmappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maruf.filmapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
